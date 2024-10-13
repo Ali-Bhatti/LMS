5cef79501d1e87081b7c2a7e8de6fbef9732d807
Muhammad Ali Bhatti <m.alibhatti55@gamil.com>
2024-10-13 20:55:36 +0500

    Fixed the issue of showing notifications in the notification panel in ADMIN PANEL

--- app/Http/Controllers/Admin/NotificationsController.php
+++ app/Http/Controllers/Admin/NotificationsController.php
@@ -24,7 +24,12 @@ class NotificationsController extends Controller
     {
         $this->authorize('admin_notifications_list');
 
-        $notifications = Notification::where('user_id', 1)
+        $user = '';
+        if (auth()->check() or apiAuth()) {
+            $user = auth()->user();
+        }
+
+        $notifications = Notification::where('user_id', $user->id)
             ->orderBy('created_at', 'desc')
             ->paginate(10);
 
